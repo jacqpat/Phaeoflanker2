2a4a146e33a9b60a9823cfcd2993724811fdd86a
jacqpat <patrick24jacques@gmail.com>
2022-05-02 09:27:16 +0200

    update to bash file

--- run_phaeoflanker.sh
+++ run_phaeoflanker.sh
@@ -14,6 +14,7 @@ file_ctg='contigs/contig_summary.tsv'
 fold_fst='../sources_fa'
 fold_gff='../sources_gff'
 fold_svd='../sources_EVEs'
+fold_bed='../BED'
 qual_ctx='hvh'
 file_svd="${fold_svd}/eves_${qual_ctx}.csv"
 size_flk="100000"
@@ -21,3 +22,4 @@ fold_svd_gff="../eves_and_flanks_gff"
 
 python3 pycode/extractEVEsData.py $file_ctg $fold_fst $fold_svd $qual_ctx
 python3 pycode/extractGenesOfEVEs.py $file_svd $fold_gff $size_flk $fold_svd_gff
+python3 pycode/getBED_of_EVEs.py $fold_bed $fold_svd_gff
