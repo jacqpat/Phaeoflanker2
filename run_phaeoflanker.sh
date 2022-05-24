ce556819aa69604e7dadb093238f2060b53e1974
jacqpat <patrick24jacques@gmail.com>
2022-05-24 16:39:32 +0200

    get fasta of genes

--- run_phaeoflanker.sh
+++ run_phaeoflanker.sh
@@ -20,20 +20,38 @@ qual_ctx='hvh'
 file_svd="${fold_svd}/eves_${qual_ctx}.csv"
 size_flk="100000"
 fold_svd_gff="../eves_and_flanks_gff"
+fold_svd_fa="../eves_and_flanks_fa"
+bed_file='${fold_bed}/bedfile_${fold_svd_gff}.bed'
 
 python3 pycode/extractEVEsData.py $file_ctg $fold_fst $fold_svd $qual_ctx
 python3 pycode/extractGenesOfEVEs.py $file_svd $fold_gff $size_flk $fold_svd_gff
 python3 pycode/getBED_of_EVEs.py $fold_bed $fold_svd_gff
 
 eves=$(cat ${file_svd} | cut -d \, -f 2)
-srcs=$(cat ${file_svd} | cut -d \, -f 6)
-for i in "${!eves[@]}"
+a_eves=()
+a_srcs=()
+for e in $eves
 do
-    src="${fold_fst}/${srcs[i]}"
-    ev="${fold_svd_gff}/EVE_${l}.gff"
-    up="${fold_svd_gff}/EVE_${l}_up.gff"
-    dw="${fold_svd_gff}/EVE_${l}_down.gff"
-    echo $src
+    a_eves+=($e)
 done
+a_eves=("${a_eves[@]:1}")
+srcs=$(cat ${file_svd} | cut -d \, -f 6)
+for s in $srcs
+do
+    a_srcs+=($s)
+done
+a_srcs=("${a_srcs[@]:1}")
 
-# bedtools getfasta -name -fi $sfst -bed $gff2 -fo $fst2
+for i in "${!a_eves[@]}"
+do
+    sc="${fold_fst}/${a_srcs[$i]}"
+    ev="${fold_svd_gff}/EVE_${a_eves[$i]}.gff"
+    up="${fold_svd_gff}/EVE_${a_eves[$i]}_up.gff"
+    dw="${fold_svd_gff}/EVE_${a_eves[$i]}_down.gff"
+    fev="${fold_svd_fa}/EVE_${a_eves[$i]}.fa"
+    fup="${fold_svd_fa}/EVE_${a_eves[$i]}_up.fa"
+    fdw="${fold_svd_fa}/EVE_${a_eves[$i]}_down.fa"
+    bedtools getfasta -name -fi $sc -bed $ev -fo $fev
+    bedtools getfasta -name -fi $sc -bed $up -fo $fup
+    bedtools getfasta -name -fi $sc -bed $dw -fo $fdw
+done
